b6e026fb540078082953c39c0c4d5c74a2f5e71c
aurelien <etu51779@henallux.be>
2025-09-18 16:43:54 +0200

    vka

--- app/components/footer.js
+++ app/components/footer.js
@@ -4,6 +4,8 @@ import '../style/footer.css'
 export default function Footer() {
   return (
     <footer>
+      <p>Pour le bien-être de votre chien! </p>
+      <img src='../favicon.ico' style={{ width: '100px',  display: 'block', margin: '0 auto' }}></img>
       <p>© 2025 T&T. Tous droits réservés.</p>
     </footer>
   );
--- app/page.js
+++ app/page.js
@@ -4,13 +4,50 @@ import './style/accueil.css';
 
 export default function HomePage() {
   return (
-    <section>
-      <div>
-        <main className="main">
-          <h1>Bienvenue sur notre site !</h1>
-          <p>Explorez nos services et notre boutique.</p>
-        </main>
-      </div>
-    </section>
+      <main className="main">
+        <section className='title'>
+          <h1>T&T, centre canin multidiscipline</h1>
+        </section>
+        <section className='description'>
+          <div className="description-content">
+            <h2>T&T, c'est quoi?</h2>
+            <p>Nous sommes un endroit pensé pour le bien-être et le dressage de votre chien ! 
+              <br/>
+              A travers différentes activités, votre chien trouvera son bonheur: bien-être, olfaction, sport, dressage,...</p>
+            <ul className='liste-services'>
+              <li>
+                <a href='/service/olfaction'>Massage</a>
+              </li>
+              <li>
+                <a href='/service/toilettage'>Toilettage</a>
+              </li>
+              <li>
+                <a href='/service/physiotherapie'>Physiothérapie</a>
+              </li>
+              <li>
+                <a href='/service/main-training'>Main-training</a>
+              </li>
+              <li>
+                <a href='/service/hooper'>Hooper</a>
+              </li>
+              <li>
+                <a href='/service/agility'>Agility</a>
+              </li>
+              <li>
+                <a href='/service/hydrotherapie'>Hydrothérapie</a>
+              </li>
+              <li>
+                <a href='/service/tapis-de-course'>Tapis de course</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className='equipe'>
+          <h2>Notre équipe</h2>
+        </section>
+        <section className='actualités'>
+          <h2>Actualités</h2>
+        </section>
+      </main>
   );
 }
